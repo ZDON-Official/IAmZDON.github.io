c701d5bf965a2c39a6fdf6978cbb546dc36fcf3d
Zohaib <zrana1214@gmail.com>
2022-11-22 12:37:53 -0500

    working on the gallery page

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ buttons.forEach(button => {
         delete active_prj.dataset.active
     })
 })
+
+
+const images = document.querySelectorAll("[data-pic]"); 
+let i = 0;
+
+setInterval(function(){ 
+
+    if(i == 0) {
+      images[i].className = "fade-in-image"
+    } else if(i == images.length ) {
+      images[i - 1].className = "fade-out-image"
+      images[0].className = "fade-in-image"
+      i = 0
+    } else {
+      images[i - 1].className = "fade-out-image"
+      images[i].className = "fade-in-image"
+    }
+
+    i++
+}, 2000)
